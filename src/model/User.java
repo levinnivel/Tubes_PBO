57966ac6e1fc431c2c88d2e52f79edfb8b177fd3
Levin <>
2020-10-12 12:34:05 +0700

    initial commit

---
+++ src/model/User.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author Andreas
+ */
+public class User {
+    private String email;
+    private String fullName;
+    private String password;
+    private String phoneNum;
+    
+    public User(){
+        
+    }
+
+    public User(String email, String fullName, String password, String phoneNum) {
+        this.email = email;
+        this.fullName = fullName;
+        this.password = password;
+        this.phoneNum = phoneNum;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getPhoneNum() {
+        return phoneNum;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setPhoneNum(String phoneNum) {
+        this.phoneNum = phoneNum;
+    }
+    
+    
+}
